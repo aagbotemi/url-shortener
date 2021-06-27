5dbc02e5200882ff46cab252505245dbb9542940
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-06-28 00:17:20 +0100

    mdi icon file

---
+++ src/utils/icons.ts
@@ -0,0 +1,27 @@
+import {
+  mdiClose,
+  mdiMenu,
+  mdiMenuDown,
+  mdiMenuRight,
+  mdiMenuUp,
+  mdiMenuLeft,
+  mdiTwitter,
+  mdiInstagram,
+  mdiFacebook,
+  mdiPinterest
+} from '@mdi/js'
+
+const icons = {
+  close: mdiClose,
+  menu: mdiMenu,
+  menuDown: mdiMenuDown,
+  menuRight: mdiMenuRight,
+  menuUp: mdiMenuUp,
+  menuLeft: mdiMenuLeft,
+  twitter: mdiTwitter,
+  instagram: mdiInstagram,
+  facebook: mdiFacebook,
+  pinterest: mdiPinterest
+}
+
+export default icons
