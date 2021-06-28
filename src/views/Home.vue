ceb356dd461af7a11afdf9baf18894b137b3559e
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-06-28 08:46:16 +0100

    jumbotron added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>Hello World</div>
+  <section>
+    <v-container>
+      <article class="jumbotron pb-16">
+        <v-row class="mt-1 mt-md-3">
+          <v-col order-md="2" md="6" cols="12">
+            <img src="../assets/img/illustration-working.svg" />
+          </v-col>
+          <v-col order-md="1" class="jumbotron-text mt-md-16" md="6" cols="12">
+            <h1>More than just shorter links</h1>
+            <p class="mt-1 mt-md-3">
+              Build your brand's recognition and get detailed insights on how
+              your links are performing.
+            </p>
+            <v-btn color="#2acfcf" class="text-capitalize white--text"
+            >Get Started</v-btn
+            >
+          </v-col>
+        </v-row>
+      </article>
+    </v-container>
+  </section>
 </template>
 
 <script lang="ts">
